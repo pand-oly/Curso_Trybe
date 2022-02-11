6ac579be86531ebb546e0381aa9e536cca2dd5eb
Rick <hoa98hoa@gmail.com>
2022-02-11 12:30:45 -0300

    Fim bloco 5 dia 2

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-2-javascript-trabalhando-com-elementos/aula/script.js
@@ -0,0 +1,17 @@
+// Acesse o elemento elementoOndeVoceEsta .
+let element = document.getElementById('elementoOndeVoceEsta')
+
+console.log(element)
+
+// Acesse pai a partir de elementoOndeVoceEsta e adicione uma color a ele.
+element.parentElement.style.backgroundColor = 'gray'
+
+// Acesse o primeiroFilhoDoFilho e adicione um texto a ele. Você se lembra dos vídeos da aula anterior, como fazer isso?
+document.getElementById('primeiroFilhoDoFilho').innerText = 'Adcionei um texto'
+
+// Acesse o primeiroFilho a partir de pai .
+document.getElementById('pai').childNodes
+// Agora acesse o primeiroFilho a partir de elementoOndeVoceEsta .
+// Agora acesse o texto Atenção! a partir de elementoOndeVoceEsta .
+// Agora acesse o terceiroFilho a partir de elementoOndeVoceEsta .
+// Agora acesse o terceiroFilho a partir de pai .
