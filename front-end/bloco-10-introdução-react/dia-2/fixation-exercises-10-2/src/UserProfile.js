cdf263c18e593d6ee10bdf78e590b22de25b892c
Rick <hoa98hoa@gmail.com>
2022-04-05 20:16:11 -0300

    fim do dia sem css por enquanto

---
+++ front-end/bloco-10-introdução-react/dia-2/fixation-exercises-10-2/src/UserProfile.js
@@ -0,0 +1,18 @@
+// arquivo UserProfile.js
+import React from 'react';
+import Image from './Image';
+
+class UserProfile extends React.Component {
+  render() {
+      const { name, email, avatar} = this.props.user;
+    return (
+      <div>
+        <p> {name} </p>
+        <p> {email} </p>
+        <Image source={avatar} alternativeText="User avatar" />
+      </div>
+    );
+  }
+}
+
+export default UserProfile;
